92a68b5b2e9ed05713b87ddb0e64a8ec60c56b1f
Jackson VonGunten <jacksonfugate@ufl.edu>
2019-01-19 13:05:29 -0500

    changed the robot program some until the programming test robot is up and ready to be programmed.

--- robot.py
+++ robot.py
@@ -9,38 +9,58 @@ class Robot(wpilib.TimedRobot):
 	def robotInit(self):
 		NetworkTables.initialize(server="10.41.18.2")
 
-		self.lf_motor = ctre.WPI_VictorSPX(0)
-		self.rf_motor = ctre.WPI_VictorSPX(1)
-		self.lb_motor = ctre.WPI_VictorSPX(2)
-		self.rb_motor = ctre.WPI_VictorSPX(3)
+		self.l_motor = ctre.WPI_VictorSPX(7)
+		self.l1_motor = ctre.WPI_VictorSPX(6)
+		self.l2_motor = ctre.WPI_VictorSPX(5)
 
-		self.lb_motor.set(ctre.ControlMode.Follower, 0)
-		self.rb_motor.set(ctre.ControlMode.Follower, 1)
+		self.l_motor.setInverted(True)
+		self.l1_motor.set(ctre.ControlMode.Follower, 7)
+		self.l2_motor.set(ctre.ControlMode.Follower, 7)
 
-		self.drive = wpilib.drive.DifferentialDrive(self.lf_motor, self.rf_motor)
+		self.r_motor = ctre.WPI_VictorSPX(4)
+		self.r1_motor = ctre.WPI_VictorSPX(3)
+		self.r2_motor = ctre.WPI_VictorSPX(2)
+
+		self.r1_motor.set(ctre.ControlMode.Follower, 4)
+		self.r2_motor.set(ctre.ControlMode.Follower, 4)
+
+		self.drive = wpilib.drive.DifferentialDrive(self.l_motor, self.r_motor)
+		self.drive.setSafetyEnabled(False)
+
+		self.controller = wpilib.XboxController(0)
 
 		self.dash = NetworkTables.getTable("dash")
+		self.lime_table = NetworkTables.getTable("limelight")
+
+		self.led = 0b0
 
 		self.lime = limelight.Limelight()
-	
-	def update(self):
-		self.lime.execute()
 
-	def align(self):
-		tx = self.dash.getNumber("tx", 0.0)
-		self.drive.arcadeDrive(0.7, tx)
+	def toggle(self):
+		if self.controller.getAButtonPressed():
+			if self.led == 0b0:
+				self.lime_table.putNumber("ledMode", 3)
+				self.led = 0b1
+			else:
+				self.lime_table.putNumber("ledMode", 1)
+				self.led = 0b0
 	
 	def autonomousInit(self):
-		self.update()
+		pass
 	
 	def autonomousPeriodic(self):
-		self.update()
+		pass
 	
 	def teleopInit(self):
-		self.update()
+		pass
 	
 	def teleopPeriodic(self):
-		self.update()
+		self.toggle()
+
+		forward = self.controller.getRawAxis(1)
+		rotate = self.controller.getRawAxis(4)
+
+		self.drive.arcadeDrive(-forward, rotate)
 
 if __name__ == '__main__':
 	wpilib.run(Robot)
