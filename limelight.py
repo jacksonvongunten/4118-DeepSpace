e9891bae93da19f2e4444af0be4c84f1b6bb1534
Jackson VonGunten <jacksonfugate@ufl.edu>
2019-02-03 11:39:53 -0500

    updaed to include elevator testing

--- limelight.py
+++ limelight.py
@@ -1,31 +1,41 @@
-from networktables import NetworkTables
 import math
+import wpilib
+from networktables import NetworkTables
 
-class Limelight:
+NetworkTables.initialize(server="10.41.18.2")
 
-    def __init__(self):
-        
-        NetworkTables.initialize(server="10.41.18.2" )
-        self.table = NetworkTables.getTable("limelight")
-        self.dash = NetworkTables.getTable("dash")
-        
-        self.setpoint = 0.0
+class AlignToTarget:
 
-        self.P = 1
-        self.D = 0
+    def __init__(self, drive, controller):
 
-        self.prev_error = 0
+        self.lime_table = NetworkTables.getTable("limelight")
 
-    def sigmoid(self, z):
-        return 1/(1 + math.e**-z)
+        self.goal = self.lime_table.getNumber("tx", 0.0)
+        self.drive = drive
+        self.controller = wpilib.XboxController(0)
+
+        try:
+            self.P = 0.7/self.goal
+            self.D = 1.0/(4*self.goal**2)
+        except ZeroDivisionError:
+            self.P = 0.0
+            self.D = 0.0
+
+        self.previous_error = 0.0
+        self.output = 0.0
 
     def PID(self):
-        error = -self.table.getNumber("tx", 0.0)
-        derivative = (error - self.prev_error) / 0.02
-        raw_adjust = self.P*error + self.D*derivative
-        self.adjust = self.sigmoid(raw_adjust)
-        self.prev_error = error
-
+        error = self.goal - self.lime_table.getNumber("tx", 0.0)
+        derivative = (error-self.previous_error)/0.02
+        self.output = (self.P*error) + (self.D*derivative)
+        self.previous_error = error
+    
     def execute(self):
         self.PID()
-        self.dash.putNumber("tx", self.adjust)
+        if abs(self.output < 0.1):
+            return None
+        else:
+            if self.goal > 0:
+                self.drive.arcadeDrive(-self.controller.getRawAxis(1), self.output)
+            else:
+                self.drive.arcadeDrive(-self.controller.getRawAxis(1), -self.output)
--- robot.py
+++ robot.py
@@ -1,6 +1,5 @@
 import wpilib
 from networktables import NetworkTables
-import limelight
 import wpilib.drive
 import ctre
 
@@ -9,58 +8,102 @@ class Robot(wpilib.TimedRobot):
 	def robotInit(self):
 		NetworkTables.initialize(server="10.41.18.2")
 
-		self.l_motor = ctre.WPI_VictorSPX(7)
-		self.l1_motor = ctre.WPI_VictorSPX(6)
-		self.l2_motor = ctre.WPI_VictorSPX(5)
+		self.l_motor = ctre.WPI_VictorSPX(1)
+		self.l1_motor = ctre.WPI_VictorSPX(3)
+		self.l2_motor = ctre.WPI_VictorSPX(6)
 
-		self.l_motor.setInverted(True)
-		self.l1_motor.set(ctre.ControlMode.Follower, 7)
-		self.l2_motor.set(ctre.ControlMode.Follower, 7)
+		self.l1_motor.set(ctre.ControlMode.Follower, 1)
+		self.l2_motor.set(ctre.ControlMode.Follower, 1)
 
-		self.r_motor = ctre.WPI_VictorSPX(4)
-		self.r1_motor = ctre.WPI_VictorSPX(3)
-		self.r2_motor = ctre.WPI_VictorSPX(2)
+		self.r_motor = ctre.WPI_VictorSPX(0)
+		self.r1_motor = ctre.WPI_VictorSPX(2)
+		self.r2_motor = ctre.WPI_VictorSPX(4)
 
-		self.r1_motor.set(ctre.ControlMode.Follower, 4)
-		self.r2_motor.set(ctre.ControlMode.Follower, 4)
+		self.r1_motor.set(ctre.ControlMode.Follower, 0)
+		self.r2_motor.set(ctre.ControlMode.Follower, 0)
 
 		self.drive = wpilib.drive.DifferentialDrive(self.l_motor, self.r_motor)
 		self.drive.setSafetyEnabled(False)
+
+		self.elevator = ctre.WPI_TalonSRX(0)
 
 		self.controller = wpilib.XboxController(0)
 
 		self.dash = NetworkTables.getTable("dash")
 		self.lime_table = NetworkTables.getTable("limelight")
 
+		self.lime_table.putNumber("camMode", 1)
+		self.lime_table.putNumber("ledMode", 1)
+
 		self.led = 0b0
 
-		self.lime = limelight.Limelight()
+	def reset(self):
+		self.drive.arcadeDrive(0.0, 0.0)
 
 	def toggle(self):
 		if self.controller.getAButtonPressed():
 			if self.led == 0b0:
+				self.lime_table.putNumber("camMode", 0)
 				self.lime_table.putNumber("ledMode", 3)
 				self.led = 0b1
 			else:
 				self.lime_table.putNumber("ledMode", 1)
+				self.lime_table.putNumber("camMode", 1)
 				self.led = 0b0
+
+	def disabledInit(self):
+		self.lime_table.putNumber("ledMode", 1)
+		self.led = 0b1
+
+	def disabledPeriodic(self):
+		self.lime_table.putNumber("ledMode", 1)
+		self.led = 0b1
 	
 	def autonomousInit(self):
-		pass
+		self.reset()
 	
 	def autonomousPeriodic(self):
 		pass
 	
+	def align(self):
+		error = self.lime_table.getNumber("tx", 0.0)
+		minCommand = 0.35
+
+		if abs(error) <= 3.0:
+			output = 0
+			self.drive.arcadeDrive(-self.controller.getRawAxis(1), 0.0)
+		else:
+			output = error/40.0
+		
+		if (output != 0 and abs(output) < minCommand):
+			output = minCommand
+			if error > 0:
+				self.drive.curvatureDrive(-self.controller.getRawAxis(1), output, False)
+			else:
+				self.drive.curvatureDrive(-self.controller.getRawAxis(1), -output, False)
+		else:
+			self.drive.curvatureDrive(-self.controller.getRawAxis(1), output, False)
+
 	def teleopInit(self):
-		pass
-	
+		self.reset()
+
 	def teleopPeriodic(self):
 		self.toggle()
 
-		forward = self.controller.getRawAxis(1)
-		rotate = self.controller.getRawAxis(4)
+		if (self.controller.getXButton() and self.lime_table.getNumber("tv", 0)):
+			self.align()
 
-		self.drive.arcadeDrive(-forward, rotate)
+		if (not self.controller.getXButton()):
+			forward = self.controller.getRawAxis(1)
+			rotate = self.controller.getRawAxis(4)
+			self.drive.curvatureDrive(-forward, rotate, False)
+
+		if (self.controller.getRawAxis(3) > 0):
+			self.elevator.set(self.controller.getRawAxis(3))
+		elif (self.controller.getRawAxis(2) > 0):
+			self.elevator.set(-self.controller.getRawAxis(2))
+		else:
+			self.elevator.set(0.0)
 
 if __name__ == '__main__':
 	wpilib.run(Robot)
