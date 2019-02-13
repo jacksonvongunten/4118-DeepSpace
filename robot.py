97573ad691281afb2a9a0e31589482de130746dd
Jackson VonGunten <jacksonfugate@ufl.edu>
2019-02-13 13:15:17 -0500

    routine commit

--- robot.py
+++ robot.py
@@ -8,22 +8,21 @@ class Robot(wpilib.TimedRobot):
 	def robotInit(self):
 		NetworkTables.initialize(server="10.41.18.2")
 
-		self.l_motor = ctre.WPI_VictorSPX(1)
-		self.l1_motor = ctre.WPI_VictorSPX(3)
-		self.l2_motor = ctre.WPI_VictorSPX(6)
+		self.left1 = ctre.WPI_VictorSPX(4)
+		self.left2 = ctre.WPI_VictorSPX(3)
+		self.left3 = ctre.WPI_VictorSPX(2)
 
-		self.l1_motor.set(ctre.ControlMode.Follower, 1)
-		self.l2_motor.set(ctre.ControlMode.Follower, 1)
+		self.left2.set(ctre.ControlMode.Follower, 4)
+		self.left3.set(ctre.ControlMode.Follower, 4)
 
-		self.r_motor = ctre.WPI_VictorSPX(0)
-		self.r1_motor = ctre.WPI_VictorSPX(2)
-		self.r2_motor = ctre.WPI_VictorSPX(4)
+		self.right1 = ctre.WPI_VictorSPX(0)
+		self.right2 = ctre.WPI_VictorSPX(1)
+		self.right3 = ctre.WPI_VictorSPX(5)
 
-		self.r1_motor.set(ctre.ControlMode.Follower, 0)
-		self.r2_motor.set(ctre.ControlMode.Follower, 0)
+		self.right2.set(ctre.ControlMode.Follower, 0)
+		self.right3.set(ctre.ControlMode.Follower, 0)
 
-		self.drive = wpilib.drive.DifferentialDrive(self.l_motor, self.r_motor)
-		self.drive.setSafetyEnabled(False)
+		self.drive = wpilib.drive.DifferentialDrive(self.left1, self.right1)
 
 		self.elevator = ctre.WPI_TalonSRX(0)
 
@@ -37,8 +36,9 @@ class Robot(wpilib.TimedRobot):
 
 		self.led = 0b0
 
-	def reset(self):
-		self.drive.arcadeDrive(0.0, 0.0)
+		self.piston = wpilib.DoubleSolenoid(0, 1)
+
+		self.limit = wpilib.DigitalInput(0)
 
 	def toggle(self):
 		if self.controller.getAButtonPressed():
@@ -51,6 +51,26 @@ class Robot(wpilib.TimedRobot):
 				self.lime_table.putNumber("camMode", 1)
 				self.led = 0b0
 
+	def fire(self):
+		if self.controller.getXButton():
+			self.piston.set(1)
+		else:
+			self.piston.set(2)
+
+	def elevate(self):
+		if (self.controller.getRawAxis(3) > 0):
+			self.elevator.set(0.5*self.controller.getRawAxis(3))
+		elif (self.controller.getRawAxis(2) > 0):
+			self.elevator.set(-self.controller.getRawAxis(2))
+		else:
+			self.elevator.set(0.0)
+
+	def drive(self):
+		forward = -self.controller.getRawAxis(1)
+		turn = self.controller.getRawAxis(4)
+
+		self.drive.curvatureDrive(forward, turn, False)
+
 	def disabledInit(self):
 		self.lime_table.putNumber("ledMode", 1)
 		self.led = 0b1
@@ -60,50 +80,19 @@ class Robot(wpilib.TimedRobot):
 		self.led = 0b1
 	
 	def autonomousInit(self):
-		self.reset()
+		pass
 	
 	def autonomousPeriodic(self):
 		pass
 	
-	def align(self):
-		error = self.lime_table.getNumber("tx", 0.0)
-		minCommand = 0.35
-
-		if abs(error) <= 3.0:
-			output = 0
-			self.drive.arcadeDrive(-self.controller.getRawAxis(1), 0.0)
-		else:
-			output = error/40.0
-		
-		if (output != 0 and abs(output) < minCommand):
-			output = minCommand
-			if error > 0:
-				self.drive.curvatureDrive(-self.controller.getRawAxis(1), output, False)
-			else:
-				self.drive.curvatureDrive(-self.controller.getRawAxis(1), -output, False)
-		else:
-			self.drive.curvatureDrive(-self.controller.getRawAxis(1), output, False)
-
 	def teleopInit(self):
-		self.reset()
+		self.drive.curvatureDrive(0.0, 0.0, False)
 
 	def teleopPeriodic(self):
+		self.drive()
+		self.elevate()
 		self.toggle()
-
-		if (self.controller.getXButton() and self.lime_table.getNumber("tv", 0)):
-			self.align()
-
-		if (not self.controller.getXButton()):
-			forward = self.controller.getRawAxis(1)
-			rotate = self.controller.getRawAxis(4)
-			self.drive.curvatureDrive(-forward, rotate, False)
-
-		if (self.controller.getRawAxis(3) > 0):
-			self.elevator.set(self.controller.getRawAxis(3))
-		elif (self.controller.getRawAxis(2) > 0):
-			self.elevator.set(-self.controller.getRawAxis(2))
-		else:
-			self.elevator.set(0.0)
+		self.fire()
 
 if __name__ == '__main__':
 	wpilib.run(Robot)
